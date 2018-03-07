5a894c2fbbdb31bd31d72177158c119159b21275
tommasoquinto <tommaso.quinto@live.it>
2018-03-07 16:33:09 +0100

    updated socket io

--- system/index.js
+++ system/index.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    $("#accedi").click(function(){
-       alert("per accedere");
-    });
+var ip = '192.168.10.112'
+var socket = io.connect(ip+":4200");
 
-    $("#registrati").click(function(){
-       alert("per registrarsi");
-    });
+$(document).ready(function(){
+
+	//socket.on('',function()...);
+	//socket.emit('','')
+
 });
