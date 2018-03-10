5ca12950b892aa39dfc7154f5590791cb13fe8a4
tommasoquinto <tommaso.quinto@live.it>
2018-03-10 18:15:18 +0100

    updated dynamic ip program

--- ideas/DynamicIp.java
+++ ideas/DynamicIp.java
@@ -1,3 +1,5 @@
+package dinamicip;
+
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,10 +9,10 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-public class Execute {
+public class DynamicIp {
 
     public static void main(String[] args) throws FileNotFoundException {
-        final String dir = System.getProperty("user.dir");
+         String dir = System.getProperty("user.dir");
        // String real = dir.replace("execute", "");
         Scanner input = new Scanner(new FileReader(dir + "/system/server.js"));
 
@@ -20,11 +22,10 @@ public class Execute {
 
         try {
             InetAddress ipAddr = InetAddress.getLocalHost();
-            ip = ipAddr.getHostAddress().toString();
+            ip = ipAddr.getHostAddress();
 
             System.out.println(ipAddr.getHostAddress());
         } catch (UnknownHostException ex) {
-            ex.printStackTrace();
         }
         a.add(right + "'" + ip + "'" + ";");
 
@@ -37,6 +38,36 @@ public class Execute {
         PrintWriter writer = new PrintWriter(dir + "/system/server.js");
         writer.print("");
         int i = 0;
+        while (i < a.size()) {
+            writer.println(a.get(i));
+            i++;
+        }
+        writer.close();
+
+       // String real = dir.replace("execute", "");
+         input = new Scanner(new FileReader(dir + "/system/js/index.js"));
+
+         a = new ArrayList<>();
+         
+                 try {
+            InetAddress ipAddr = InetAddress.getLocalHost();
+            ip = ipAddr.getHostAddress();
+
+            System.out.println(ipAddr.getHostAddress());
+        } catch (UnknownHostException ex) {
+        }
+
+        a.add(right + "'" + ip + "'" + ";");
+
+        while (input.hasNextLine()) {
+            a.add(input.nextLine());
+        }
+        input.close();
+        a.remove(1);
+
+        writer = new PrintWriter(dir + "/system/js/index.js");
+        writer.print("");
+          i = 0;
         while (i < a.size()) {
             writer.println(a.get(i));
             i++;
--- system/js/index.js
+++ system/js/index.js
@@ -1,11 +1,16 @@
-var ip = 'localhost'
+var ip='192.168.1.9';
 var socket = io.connect(ip+":4200");
 
-$(document).ready(function()
-{
+
+$(document).ready(function() {
+
 	$('#openMenu').on('click',function()
 	{
 		$('.ui.labeled.icon.sidebar').sidebar('toggle');
+
+
 	});
-	
-});
+
+})();
+
+
